54d25cc9223abe9aede09888ba50523e772c07f3
Kyle Bebak <kyle@elementaryml.com>
2023-12-18 22:29:05 -0600

    Add queries/lua/symbols.scm

---
+++ queries/lua/symbols.scm
@@ -0,0 +1,5 @@
+(function_declaration name: (identifier) @definition.function.bc.1)
+(function_declaration name: (method_index_expression) @definition.function.bc.1)
+
+(chunk (variable_declaration (assignment_statement (variable_list name: (identifier) @definition.var))))
+(chunk (assignment_statement (variable_list name: (identifier) @definition.var)))
